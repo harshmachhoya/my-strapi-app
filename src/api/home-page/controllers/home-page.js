1af89d64e9f8ea8e470974f7d64745428a97d5fb
harshmachhoya <harsh.machhoya@travsport.se>
2022-05-05 20:12:28 +0530

    Added contact card

--- src/api/home-page/controllers/home-page.js
+++ src/api/home-page/controllers/home-page.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 /**
  *  home-page controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::home-page.home-page');
+module.exports = createCoreController("api::home-page.home-page");
